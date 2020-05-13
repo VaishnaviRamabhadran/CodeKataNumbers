3d787ab12a6714f4d4465652001cd17e06ebb666
VaishnaviRamabhadran <vaishu2395@gmail.com>
2020-05-13 12:50:39 +0530

    Create CodeKataNumbers1.java
    CodeKataNumbers1

---
+++ CodeKataNumbers1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class CodeKataNumbers{
+public static void main(String[] args)
+{
+int n,i,count=0;
+Scanner sc=new Scanner(System.in);
+n=sc.nextInt();
+for(i=1;i<n;i++)
+{
+ if(i+(n-i)==n)
+ { System.out.println(i+"+"+(n-i)+" ="+n);
+  count++;}
+}
+
+
+System.out.print(count);
+}
+}
